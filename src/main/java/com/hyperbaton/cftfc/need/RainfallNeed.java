4864d2adc8101bd40ae30f5063760db9394a933d
Pablo J Blasco <pablojblasco@gmail.com>
2026-06-23 09:13:52 +0200

    Update for CFT v2.1.0

--- src/main/java/com/hyperbaton/cftfc/need/RainfallNeed.java
+++ src/main/java/com/hyperbaton/cftfc/need/RainfallNeed.java
@@ -6,8 +6,15 @@ import com.hyperbaton.cftfc.CftfcRegistry;
 import com.hyperbaton.cftfc.need.satisfaction.RainfallNeedSatisfier;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.List;
+import java.util.Optional;
 
 public class RainfallNeed extends Need {
+    private static final ResourceLocation DEFAULT_ICON = ResourceLocation.fromNamespaceAndPath("tfc", "wooden_bucket");
+
     public static final Codec<RainfallNeed> RAINFALL_NEED_CODEC = RecordCodecBuilder.create(instance -> instance.group(
             Codec.STRING.fieldOf("id").forGetter(RainfallNeed::getId),
             Codec.DOUBLE.fieldOf("damage").forGetter(RainfallNeed::getDamage),
@@ -16,6 +23,7 @@ public class RainfallNeed extends Need {
             Codec.DOUBLE.fieldOf("satisfaction_threshold").forGetter(RainfallNeed::getSatisfactionThreshold),
             Codec.DOUBLE.fieldOf("frequency").forGetter(RainfallNeed::getFrequency),
             Codec.BOOL.optionalFieldOf("hidden", DEFAULT_HIDDEN).forGetter(RainfallNeed::isHidden),
+            ResourceLocation.CODEC.optionalFieldOf("icon").forGetter(Need::getIcon),
             Codec.DOUBLE.fieldOf("min_rainfall").forGetter(RainfallNeed::getMinRainfall),
             Codec.DOUBLE.fieldOf("max_rainfall").forGetter(RainfallNeed::getMaxRainfall)
     ).apply(instance, RainfallNeed::new));
@@ -25,8 +33,9 @@ public class RainfallNeed extends Need {
 
     public RainfallNeed(String id, double damage, double damageThreshold, double providedHappiness,
                         double satisfactionThreshold, double frequency, boolean hidden,
+                        Optional<ResourceLocation> icon,
                         double minRainfall, double maxRainfall) {
-        super(id, damage, damageThreshold, providedHappiness, satisfactionThreshold, frequency, hidden);
+        super(id, damage, damageThreshold, providedHappiness, satisfactionThreshold, frequency, hidden, icon);
         this.minRainfall = minRainfall;
         this.maxRainfall = maxRainfall;
     }
@@ -37,13 +46,17 @@ public class RainfallNeed extends Need {
     }
 
     @Override
-    public NeedSatisfier<? extends Need> createSatisfier() {
-        return createSatisfier(this.getSatisfactionThreshold(), false);
+    public NeedSatisfier<? extends Need> createSatisfier(double satisfaction, boolean isSatisfied) {
+        return new RainfallNeedSatisfier(satisfaction, isSatisfied, this);
     }
 
     @Override
-    public NeedSatisfier<? extends Need> createSatisfier(double satisfaction, boolean isSatisfied) {
-        return new RainfallNeedSatisfier(satisfaction, isSatisfied, this);
+    public String getTypeName() {
+        return Component.translatable("gui.cftfc.need_type.rainfall").getString();    }
+
+    @Override
+    public List<ResourceLocation> getDefaultIcons() {
+        return List.of(DEFAULT_ICON);
     }
 
     public double getMinRainfall() {
--- src/main/java/com/hyperbaton/cftfc/need/TemperatureNeed.java
+++ src/main/java/com/hyperbaton/cftfc/need/TemperatureNeed.java
@@ -6,10 +6,15 @@ import com.hyperbaton.cftfc.CftfcRegistry;
 import com.hyperbaton.cftfc.need.satisfaction.TemperatureNeedSatisfier;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 
+import java.util.List;
 import java.util.Optional;
 
 public class TemperatureNeed extends Need {
+    private static final ResourceLocation DEFAULT_ICON = ResourceLocation.fromNamespaceAndPath("tfc", "thermometer");
+
     public static final Codec<TemperatureNeed> TEMPERATURE_NEED_CODEC = RecordCodecBuilder.create(instance -> instance.group(
             Codec.STRING.fieldOf("id").forGetter(TemperatureNeed::getId),
             Codec.DOUBLE.fieldOf("damage").forGetter(TemperatureNeed::getDamage),
@@ -18,6 +23,7 @@ public class TemperatureNeed extends Need {
             Codec.DOUBLE.fieldOf("satisfaction_threshold").forGetter(TemperatureNeed::getSatisfactionThreshold),
             Codec.DOUBLE.fieldOf("frequency").forGetter(TemperatureNeed::getFrequency),
             Codec.BOOL.optionalFieldOf("hidden", DEFAULT_HIDDEN).forGetter(TemperatureNeed::isHidden),
+            ResourceLocation.CODEC.optionalFieldOf("icon").forGetter(Need::getIcon),
             Codec.DOUBLE.fieldOf("min_average_temperature").forGetter(TemperatureNeed::getMinAverageTemperature),
             Codec.DOUBLE.fieldOf("max_average_temperature").forGetter(TemperatureNeed::getMaxAverageTemperature),
             SeasonalTemperatureLimits.SEASONAL_TEMPERATURE_LIMITS_CODEC.optionalFieldOf("seasonal_limits")
@@ -30,9 +36,10 @@ public class TemperatureNeed extends Need {
 
     public TemperatureNeed(String id, double damage, double damageThreshold, double providedHappiness,
                            double satisfactionThreshold, double frequency, boolean hidden,
+                           Optional<ResourceLocation> icon,
                            double minAverageTemperature, double maxAverageTemperature,
                            Optional<SeasonalTemperatureLimits> seasonalLimits) {
-        super(id, damage, damageThreshold, providedHappiness, satisfactionThreshold, frequency, hidden);
+        super(id, damage, damageThreshold, providedHappiness, satisfactionThreshold, frequency, hidden, icon);
         this.minAverageTemperature = minAverageTemperature;
         this.maxAverageTemperature = maxAverageTemperature;
         this.seasonalLimits = seasonalLimits;
@@ -44,13 +51,18 @@ public class TemperatureNeed extends Need {
     }
 
     @Override
-    public NeedSatisfier<? extends Need> createSatisfier() {
-        return createSatisfier(this.getSatisfactionThreshold(), false);
+    public NeedSatisfier<? extends Need> createSatisfier(double satisfaction, boolean isSatisfied) {
+        return new TemperatureNeedSatisfier(satisfaction, isSatisfied, this);
     }
 
     @Override
-    public NeedSatisfier<? extends Need> createSatisfier(double satisfaction, boolean isSatisfied) {
-        return new TemperatureNeedSatisfier(satisfaction, isSatisfied, this);
+    public String getTypeName() {
+        return Component.translatable("gui.cftfc.need_type.temperature").getString();
+    }
+
+    @Override
+    public List<ResourceLocation> getDefaultIcons() {
+        return List.of(DEFAULT_ICON);
     }
 
     public double getMinAverageTemperature() {
